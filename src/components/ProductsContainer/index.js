8b27f1be0b8f7628ad69e9b47b818e927fd9fcdb
Müberra <55498680+muberracatak@users.noreply.github.com>
2023-02-20 21:52:12 +0300

    Add files via upload

--- src/components/ProductsContainer/index.js
+++ src/components/ProductsContainer/index.js
@@ -1,1 +1,27 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import ProductItem from '../../components/ProductItem'
+import productsGetir from '../../../data/productsGetir.json'
 
+const DATA = productsGetir.products;
+const index = () => {
+    return (
+        <View>
+            <View style={{ flexDirection: 'row', alignItems: 'center', backgroundColor: 'white' }}>
+                {DATA.slice(0, 2).map((item) => (
+                    <ProductItem key={item.id} item={item} />
+                ))}
+            </View>
+            <Text style={{ color: 'gray', fontWeight: 'bold', padding: 14 }}>Çubuk</Text>
+            <View style={{
+                flexDirection: 'row', alignItems: 'center', flexWrap: 'wrap', flex: 1, backgroundColor: 'white', paddingVertical: 10
+            }}>
+                {DATA.slice(2).map((item) => (
+                    <ProductItem key={item.id} item={item} />
+                ))}
+            </View>
+        </View>
+    )
+}
+
+export default index
