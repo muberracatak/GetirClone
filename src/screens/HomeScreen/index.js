b95fa7f18672780e31d15cc09a09b937015a45b0
Müberra <55498680+muberracatak@users.noreply.github.com>
2023-02-20 21:57:34 +0300

    Add files via upload

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,1 +1,19 @@
+import { View, Text, Image, ScrollView } from 'react-native'
+import React from 'react'
+import styles from './styles'
+import { Entypo } from '@expo/vector-icons';
+import HeaderMain from '../../components/HeaderMain'
+import BannerCarousel from '../../components/BannerCarousel'
+import MainCategories from '../../components/MainCategories'
 
+const index = () => {
+    return (
+        <ScrollView stickyHeaderIndices={[0]} style={{ backgroundColor: '#f5f5f5' }}>
+            <HeaderMain />
+            <BannerCarousel />
+            <MainCategories />
+        </ScrollView>
+    )
+}
+
+export default index
